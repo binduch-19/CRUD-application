8e8489242d55853950ef169199dde9466799e2f6
Bindu <binduchinnakondu89@gmail.com>
2024-08-09 10:34:20 +0530

    first commit

---
+++ src/webpack.config.js
@@ -0,0 +1,11 @@
+// webpack.config.js
+const webpack = require('webpack');
+
+module.exports = {
+  // ... other configurations
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ],
+};
